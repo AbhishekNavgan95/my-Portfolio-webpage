1aaa832a6d77c428dca09181615cf9851a073565
Abhishek Navgan <navganabhishek90@gmail.com>
2023-09-07 03:22:23 +0000

    fixed locomitve scroll bug

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 cursortracker();
 function cursortracker() {
   window.addEventListener("mousemove", function (dets) {
-    // document.querySelector(
-    //   ".cursor-tracker"
-    // ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
     document.querySelector(
       ".cursor"
     ).style.transform = `translate(${dets.clientX-5}px, ${dets.clientY-4}px)`;
@@ -14,7 +11,9 @@ function cursortracker() {
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".main"),
   smooth: true,
+  getDirection: true
 });
+
 locoScroll.on("scroll", ScrollTrigger.update);
 
 ScrollTrigger.scrollerProxy(".main", {
@@ -39,29 +38,3 @@ ScrollTrigger.scrollerProxy(".main", {
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 ScrollTrigger.refresh();
-
-gsap.from(".intro-heading-1", {
-  x: "120%",
-  scrollTrigger: {
-    trigger: ".intro-heading-1",
-    scroller: ".main",
-    // markers: true,
-    scrub: 2,
-  },
-});
-
-gsap.from(".intro-heading-2", {
-  x: "-120%",
-  scrollTrigger: {
-    trigger: ".intro-heading-2",
-    scroller: ".main",
-    // markers: true,
-    scrub: 2,
-  },
-});
-
-gsap.from(".float", {
-  duration: 1,
-  opacity: 0,
-  delay: 1,
-});
