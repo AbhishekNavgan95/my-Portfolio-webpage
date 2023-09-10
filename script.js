11668c5b093c21f1b87608dd37b404d6c6c64d51
AbhishekNavgan95 <navganabhishek90@gmail.com>
2023-09-10 20:15:37 +0530

    added styling

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 cursortracker();
 function cursortracker() {
   window.addEventListener("mousemove", function (dets) {
-    document.querySelector(
-      ".cursor"
-    ).style.transform = `translate(${dets.clientX-5}px, ${dets.clientY-4}px)`;
+    document.querySelector(".cursor").style.transform = `translate(${
+      dets.clientX - 6
+    }px, ${dets.clientY - 6}px)`;
+    document.querySelector(".cursor-tracker-2").style.transform = `translate(${
+      dets.clientX - 150
+    }px, ${dets.clientY - 150}px)`;
   });
 }
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".main"),
   smooth: true,
-  getDirection: true
 });
 
 locoScroll.on("scroll", ScrollTrigger.update);
@@ -38,3 +40,98 @@ ScrollTrigger.scrollerProxy(".main", {
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 ScrollTrigger.refresh();
+
+gsap.from(".page-4", {
+  delay: 5,
+  scrollTrigger: {
+    trigger: ".skills-sec",
+    scroller: ".main",
+    start: "top 80%",
+    // markers: true,
+    onEnter: function () {
+      skillsProgress();
+    },
+  },
+});
+
+// skillsProgress();
+function skillsProgress() {
+  let percentage = [79, 49, 94, 64, 69, 39];
+  let progressPercentage = document.querySelectorAll(".progress-percentage");
+  let progressBar = document.querySelectorAll(".progress");
+  let i = 0;
+  progressBar.forEach((bar) => {
+    bar.style.transform = `translateX(${percentage[i]}%)`;
+    i++;
+  });
+}
+
+// data and time
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+let month, day, hours, minutes, seconds, ampm, formattedDate;
+setTime();
+
+setInterval(() => {
+  setTime();
+}, 60000); // 60,000
+
+function setTime() {
+  currentDate = new Date();
+  month = monthNames[currentDate.getMonth()];
+  day = String(currentDate.getDate()).padStart(2, "0");
+  hours = String(currentDate.getHours() % 12 || 12).padStart(2, "0"); // Convert to 12-hour format
+  minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  ampm = currentDate.getHours() >= 12 ? "pm" : "am";
+
+  formattedDate = `${month} ${day} ${hours}:${minutes}${ampm}`;
+  document.querySelector(".time").innerHTML = "// " + formattedDate;
+}
+
+
+movebgelem();
+setInterval(movebgelem, 2000);
+function movebgelem() {
+  let elem1 = document.querySelector(".bg-elem-1");
+  let elem2 = document.querySelector(".bg-elem-2");
+  let elem3 = document.querySelector(".bg-elem-3");
+  let elem4 = document.querySelector(".bg-elem-4");
+  let num1, num2;
+  num1 = Math.floor(Math.random() * 30);
+  num2 = Math.floor(Math.random() * 70);
+  num3 = Math.floor(Math.random() * 110);
+  num4 = Math.floor(Math.random() * 30);
+  elem1.style.transform = `translate(${num3}rem, ${num1}rem)`;
+  elem2.style.transform = `translate(${num3}rem, -${num4}rem)`;
+  elem3.style.transform = `translate(${num1}rem, -${num2}rem)`;
+  elem4.style.transform = `translate(${num1}rem, ${num3}rem)`;
+}
+
+
+let skills = document.querySelectorAll(".skill");
+let preview = document.querySelector(".preview-sec")
+skills.forEach((e) => {
+  e.addEventListener("mouseover", () => {
+    const skillsTitleElement = e.querySelector('.skills-title');
+    const skillsTitleInnerText = skillsTitleElement.innerText;
+    preview.innerText = skillsTitleInnerText
+    preview.style.transform = "translate(-50%, -50%) scaleX(1)";
+  })
+  e.addEventListener("mouseleave", () => {
+    preview.innerText = "";
+    preview.style.transform = "translate(-50%, -50%) scaleX(0)";
+  })
+})
